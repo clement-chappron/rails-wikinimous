e46249fac2d24601c667d809e5fa16bb92ea722f
clement-chappron <clement.chappron@gmail.com>
2024-07-27 17:56:14 +0400

    modifiactions du frtont avec bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+(2..20).each do |id|
+  Article.create!(
+    title: Faker::Hacker.adjective,
+    content: Faker::Hacker.say_something_smart
+  )
+end
